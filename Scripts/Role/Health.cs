c10b1f96f280a059f6bb7f6f8048159e443031fb
1 <1>
2014-11-07 15:14:53 +0800

    1

---
+++ Scripts/Role/Health.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+		public int maxHealth = 100;
+		public int curHealth = 100;
+
+		public void ChangeHealth (int value)
+		{
+				curHealth = Mathf.Max (0, curHealth + value);
+				curHealth = Mathf.Min (curHealth, maxHealth);
+		}
+}
---
+++ Scripts/UI/ShowHealthBar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowHealthBar : MonoBehaviour
+{
+		private float healthBarWidth;
+		private float healthBarHeight = 20f;
+		private GameObject player;
+		private Health playHealth;
+		public GameObject selectedEnemy;
+		private Health enemyHealth;
+
+		void Awake ()
+		{
+				healthBarWidth = Screen.width / 5; 
+				player = GameObject.FindGameObjectWithTag ("Player");
+				playHealth = player.GetComponent<Health> ();
+		}
+	
+		void OnGUI ()
+		{
+				//player health
+				GUI.Box (new Rect (10, 10, healthBarWidth * playHealth.curHealth / playHealth.maxHealth, healthBarHeight), playHealth.curHealth + "/" + playHealth.maxHealth);
+				//enemy health
+				if (selectedEnemy != null) {
+						enemyHealth = selectedEnemy.GetComponent<Health> ();
+						GUI.Box (new Rect (Screen.width / 2 - healthBarWidth / 2, 10, healthBarWidth * enemyHealth.curHealth / enemyHealth.maxHealth, healthBarHeight), enemyHealth.curHealth + "/" + enemyHealth.maxHealth);
+				}
+		}
+}
